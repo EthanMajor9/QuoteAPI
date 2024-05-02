c68fb4dd0f874478e07fd926b33c8ece6f34d6ff
Ethan Major <ethanmajor9@gmail.com>
2024-05-02 12:56:27 -0400

    API can now delete and add quotes to the database

--- src/quotesController.js
+++ src/quotesController.js
@@ -11,11 +11,32 @@ const getAllQuotes = (req, res) => {
 };
 
 const addQuote = (req, res) => {
+	const {author, text, timestamp} = req.body;
+	const query = 'INSERT into quotes (author, quote_text, timestamp) values (?, ?, ?)';
 
+	dbConnection.query(query, [author, text, timestamp], (err) => {
+		if(err) {
+			return res.status(500).send({error: err});
+		}
+
+		res.status(200).send({success: 'Quote inserted into the database successfully'});
+	});
 };
 
 const deleteQuote = (req, res) => {
+	const idParam = req.params['id'];
+	const quoteId = idParam.substring(idParam.indexOf(':') + 1);
 
+	console.log('Requesting deletion of quote with id', quoteId);
+
+	const query = `DELETE from quotes where id=?`;
+	dbConnection.query(query, [quoteId], (err) => {
+		if(err) {
+			return res.status(500).send({error: err});
+		}
+
+		res.status(200).send({success: 'Quote deleted from the database successfully'});
+	});
 };
 
 const updateQuote = (req, res) => {
